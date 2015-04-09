2ad64f118f3ce0b9f6e3a0ced38a5d3f36a9c54e
Joe McPherson <j.m.p.mcpherson@gmail.com>
2015-04-09 13:38:31 +0100

    refactored and number of messages in group returned

--- includes/ecv.php
+++ includes/ecv.php
@@ -1,5 +1,8 @@
 <?php
-function eldis_solr_search_xml($query_string = ''){
+/*
+ * Makes a call to the Eldis solr webservice passing query string and returns Simple XML Object
+ */
+function ecv_eldis_solr_search_xml($query_string = ''){
 	$source_base_url = 'http://ec.solr.test.ids.ac.uk';
 	if($query_string){
 		$source_url = $source_base_url . '/?q=' . $query_string;
@@ -8,14 +11,64 @@ function eldis_solr_search_xml($query_string = ''){
 	$xmlobj = new SimpleXMLElement($xmlstr);
 	return $xmlobj;
 }
-/* get all group names and ids */
-$xmlobj = eldis_solr_search_xml('entity_type:group&fl=entity_id+entity_name&rows=1000');
 
-$group_options = '';
-if(isset($xmlobj->result->doc)){
-	foreach($xmlobj->result->doc as $doc){
-		$group_options['' . $doc->str[0]] = trim('' . $doc->str[1]);
+/*
+ * Gets all the group names and ids
+ */
+function ecv_get_eldis_solr_group_data(){
+	$xmlobj = ecv_eldis_solr_search_xml('entity_type:group&fl=entity_id+entity_name&rows=1000');
+	$group_options = array();
+	if(isset($xmlobj->result->doc)){
+		foreach($xmlobj->result->doc as $doc){
+			$group_options['' . $doc->str[0]] = trim('' . $doc->str[1]);
+		}
 	}
-} else {
-	$output .= 'Failed to load groups';
+	return $group_options;	
 }
+
+/*
+ * Builds the solr base query from the user filters
+ */
+function ecv_build_base_query($group_options){
+	$query = '';
+	if(isset($_REQUEST['submit'])){
+		if(isset($_REQUEST['group'])){
+			$group_key = $_REQUEST['group'];
+			$group_header = $group_options[$group_key];
+			$query .= 'group_id:' . $group_key;
+		}
+	}	
+	return $query;
+}
+
+/*
+ * Gets group name from filters
+ */
+function ecv_get_group_name($group_options){
+	$group_name = '';
+	if(isset($_REQUEST['submit'])){
+		if(isset($_REQUEST['group'])){
+			$group_key = $_REQUEST['group'];
+			$group_name = $group_options[$group_key];
+		}
+	}	
+	return $group_name;
+}
+
+/*
+ * Gets an array of data for the page (populated with form is submittes and solr is queried
+ */
+function ecv_load_page_data(){
+	$page_vars = array();
+	$group_options= ecv_get_eldis_solr_group_data();
+	$base_query = ecv_build_base_query($group_options);
+	$page_vars['group_options'] = $group_options;
+	$page_vars['group_name'] = ecv_get_group_name($group_options);
+	$group_number_of_messages_xml = ecv_eldis_solr_search_xml($base_query . ' AND entity_type:message');
+	$group_number_of_messages_result_attributes = $group_number_of_messages_xml->result->attributes();
+	$group_number_of_messages = $group_number_of_messages_result_attributes['numFound'];
+	$page_vars['group_number_of_messages'] = $group_number_of_messages;
+	return $page_vars;	
+}
+
+
--- index.php
+++ index.php
@@ -1,62 +1,71 @@
 <?php
 include_once('includes/ecv.php');
 $this_page_url =  "//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$page_vars = ecv_load_page_data();
 ?>
 <html>
 <head>
 <title>Eldis Communities Visualisation</title>
-<link rel="stylesheet"href="css/html5reset-1.6.1.css"/>
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet"href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css"/>
-<link rel="stylesheet"href="css/ecv.css"/>
+<link rel="stylesheet" href="css/html5reset-1.6.1.css" />
+<link
+	href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"
+	rel="stylesheet">
+<link rel="stylesheet"
+	href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" />
+<link rel="stylesheet" href="css/ecv.css" />
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 <script src="js/ecv.js"></script>
 </head>
 <body>
 	<div class="container">
-			<div class="row">
-				<div id="heading-row" class="col-md-12">
-					<h1>Eldis Communities Visualisation</h1>	
-				</div>
-				
-				<div id="user-filters" class="col-md-6">
-					<form action="<?php echo $this_page_url; ?>" method="get">
-						<div class="ui-widget">
-							<label for="group">Group</label>
-							<select id="group" name="group">
+		<div class="row">
+			<div id="heading-row" class="col-md-12">
+				<h1>Eldis Communities Visualisation</h1>
+			</div>
+
+			<div id="user-filters" class="col-md-6">
+				<form action="<?php echo $this_page_url; ?>" method="get">
+					<div class="ui-widget">
+						<label for="group">Group</label> <select id="group" name="group">
 							<option value=""></option>
 							<?php
-							foreach($group_options as $group_key => $group_name):
+							foreach($page_vars['group_options'] as $group_key => $group_name):
 							?>
-								<option value="<?php echo $group_key; ?>"><?php echo $group_name; ?></option>
+							<option value="<?php echo $group_key; ?>"><?php echo $group_name; ?></option>
 							<?php
 							endforeach;
 							?>
-							</select>
-						</div>
-						
-						<div class="ui-widget">
-							<label for="start_date">Start date</label>
-							<input class="datepicker" name="start_date" id="start_date" type="text"/>
-						</div>		
-						
-						<div class="ui-widget">
-							<label for="end_date">End date</label>
-							<input class="datepicker" name="end_date" id="end_date" type="text"/>
-						</div>		
-						
-						<div class="ui-widget">
-							<label for="include_admin">Admin included</label>
-							<input name="include_admin" id="include_admin" type="checkbox" value="1" checked="checked"/>
-						</div>
-						<input name="submit" id="submit" type="submit" value="Submit"/>
-					</form>
-				</div>
-				<div id="top-level-results" class="col-md-6">
-					<p>feedback</p>
+						</select>
+					</div>
+
+					<div class="ui-widget">
+						<label for="start_date">Start date</label> <input
+							class="datepicker" name="start_date" id="start_date" type="text" />
+					</div>
+
+					<div class="ui-widget">
+						<label for="end_date">End date</label> <input class="datepicker"
+							name="end_date" id="end_date" type="text" />
+					</div>
+
+					<div class="ui-widget">
+						<label for="include_admin">Admin included</label> <input
+							name="include_admin" id="include_admin" type="checkbox" value="1"
+							checked="checked" />
+					</div>
+					<input name="submit" id="submit" type="submit" value="Submit" />
+				</form>
+			</div>
+			<div id="top-level-results" class="col-md-6">
+				<h2>
+				<?php echo $page_vars['group_name'] ?>
+				</h2>
+				<div class="results">
+					<span class="labelspan">Number of messages: </span><span class="result"><?php echo $page_vars['group_number_of_messages'] ?></span>
 				</div>
 			</div>
+		</div>
 	</div>
 </body>
 </html>
